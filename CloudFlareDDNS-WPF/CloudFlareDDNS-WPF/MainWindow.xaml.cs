858885dd665322b0903f47d5757efaf2309ee59b
JeffersonQin <1247006353@qq.com>
2020-07-12 23:51:23 +0800

    Console UI以及Bug Fixes

--- CloudFlareDDNS-WPF/CloudFlareDDNS-WPF/MainWindow.xaml.cs
+++ CloudFlareDDNS-WPF/CloudFlareDDNS-WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.IconPacks;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -22,10 +23,16 @@ namespace CloudFlareDDNS_WPF
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        // 包含所有页面的一个字典
+        private Dictionary<string, Uri> ddnsViews = new Dictionary<string, Uri>();
+
         public MainWindow()
         {
             InitializeComponent();
             this.Title = CfDdnsProperty.SHOW_NAME;
+            ddnsViews.Add("Configuration", new Uri("Pages/ConfigurationPage.xaml", UriKind.Relative));
+            ddnsViews.Add("Console", new Uri("Pages/ConsolePage.xaml", UriKind.Relative));
+            navigationFrame.Navigate(ddnsViews["Configuration"]);
         }
 
         private void LaunchGitHubSite(object sender, RoutedEventArgs e)
@@ -45,6 +52,35 @@ namespace CloudFlareDDNS_WPF
             aboutWindow.Show();
         }
 
+        private void ShowConfiguration(object sender, RoutedEventArgs e)
+        {
+            // Navigate
+            navigationFrame.Navigate(ddnsViews["Configuration"]);
+            // TODO：密码框载入（Navigate后会消失）
+        }
+
+        private void ShowConsole(object sender, RoutedEventArgs e)
+        {
+            // Navigate
+            navigationFrame.Navigate(ddnsViews["Console"]);
+            // TODO：密码框载入（Navigate后会消失）
+        }
+
+        private void DDNSOperation(object sender, RoutedEventArgs e)
+        {
+            
+            if ((string)DDNSButton.Content == "▶  Start DDNS")
+            {
+                DDNSButton.Content = "█  Stop DDNS";
+                DDNSButton.Background = new SolidColorBrush(Color.FromRgb(205, 92, 92));
+                DDNSButton.BorderBrush = new SolidColorBrush(Color.FromRgb(205, 92, 92));
+            } else
+            {
+                DDNSButton.Content = "▶  Start DDNS";
+                DDNSButton.Background = new SolidColorBrush(Color.FromRgb(60, 179, 113));
+                DDNSButton.BorderBrush = new SolidColorBrush(Color.FromRgb(60, 179, 113));
+            }
+        }
 
     }
 }
