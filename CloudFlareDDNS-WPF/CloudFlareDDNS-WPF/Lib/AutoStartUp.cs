a406f2e790de6dbba51cb8cd719a7a759ea711ec
JeffersonQin <1247006353@qq.com>
2020-08-05 12:06:28 +0800

    Create AutoStartUp.cs
    Dealing with start up

---
+++ CloudFlareDDNS-WPF/CloudFlareDDNS-WPF/Lib/AutoStartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudFlareDDNS_WPF.Lib
+{
+    class AutoStartUp
+    {
+        void start() { 
+        }
+    }
+}
